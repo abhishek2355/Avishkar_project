2bbaed5660ba94feaadeb6f8407e61e150973e78
Abhishek Santosh Raut <90473454+abhishek2355@users.noreply.github.com>
2023-10-18 20:12:58 +0530

    Added constant folder in app. (#1)

---
+++ lib/Constants/app_cookie_values.dart
@@ -0,0 +1,3 @@
+/// This file is dedicated to list down the keys of all cookies.
+
+const userLoggedInIDCookieKey = 'user-logged-in-id';
---
+++ lib/Constants/app_widths.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:avishkar/Constants/app_design.dart' as app_design;
+
+var appDesignWidth = app_design.designWidth;
+
+final double fullyExpendedWidth = 1.sw;
+final double halfExpandedWidth = 0.5.sw;
+final double width1 = 1 / appDesignWidth;
+final double width2 = 2 / appDesignWidth;
+final double width10 = 10 / appDesignWidth;
+final double width15 = 15 / appDesignWidth;
+final double width19 = 19 / appDesignWidth;
+final double width16 = 16 / appDesignWidth;
+final double width17 = 17 / appDesignWidth;
+final double width22 = 22 / appDesignWidth;
+final double width25 = 25 / appDesignWidth;
+final double width28 = 28 / appDesignWidth;
+final double width30 = 30 / appDesignWidth;
+final double width40 = 40 / appDesignWidth;
+final double width50 = 50 / appDesignWidth;
+final double width51 = 51 / appDesignWidth;
+final double width56 = 56 / appDesignWidth;
+final double width57 = 57 / appDesignWidth;
+final double width59 = 59 / appDesignWidth;
+final double width108 = 108 / appDesignWidth;
+final double width120 = 120 / appDesignWidth;
+final double width130 = 130 / appDesignWidth;
+final double width190 = 190 / appDesignWidth;
+final double width286 = 286 / appDesignWidth;
+final double width288 = 288 / appDesignWidth;
+final double width368 = 368 / appDesignWidth;
