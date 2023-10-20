f1dfecda482aeeb37f3fdaf18c73d74299ebde87
Vishal Raju Ambekar <127341095+vishalambekar37@users.noreply.github.com>
2023-10-21 01:12:25 +0530

    Added register Page. (#7)

--- lib/Screen/Pages/home.dart
+++ lib/Screen/Pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:avishkar/Screen/Authentication/apis/authentication_api.dart';
+import 'package:avishkar/Screen/Authentication/widget/login.dart';
+import 'package:avishkar/Screen/Pages/registration_form/registrationForm.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:avishkar/Constants/app_heights.dart' as app_heights;
@@ -24,22 +26,43 @@ class _HomeScreenState extends State<HomeScreen> {
     // Accessing MediaQuery for responsive layout
     // Calculate the height and width of the screen.
     var media = MediaQuery.of(context);
-    final double screenHeight = media.size.height - media.padding.top - media.padding.bottom;
-    final double screenWidth = media.size.width - media.padding.left - media.padding.right;
+    final double screenHeight =
+        media.size.height - media.padding.top - media.padding.bottom;
+    final double screenWidth =
+        media.size.width - media.padding.left - media.padding.right;
+
+    // Function for logout the user
+    signOut() {
+      SignUpApis.logOut();
+    }
 
     // Main body of the home screen
     return SafeArea(
       child: Scaffold(
         body: SingleChildScrollView(
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: screenWidth * app_widths.width16),
+            padding: EdgeInsets.symmetric(
+                horizontal: screenWidth * app_widths.width16),
             child: Column(
               children: [
-                // Row for the 
+                // Row for the
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    // Display user's mail id...
+                    Text(
+                      SignUpApis.user!.email.toString(),
+                      style: TextStyle(
+                          fontSize: screenHeight * app_heights.height20),
+                    ),
+                    // Icons for logout user
+                    IconButton(
+                      onPressed: () async {
+                        await signOut();
+                      },
+                      icon: Icon(
+                        Icons.logout_outlined,
+                        size: screenHeight * app_heights.height20,
+                      ),
                     Text(user!.email.toString(), style: TextStyle(fontSize: screenHeight * app_heights.height20),),
                     // Icons for logout user
                     IconButton(
@@ -50,7 +73,17 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                   ],
                 ),
-                
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => RegisrationFormPage(),
+                          ));
+                    },
+                    child: Text(
+                      "Hello",
+                    ))
               ],
             ),
           ),
---
+++ lib/Screen/Pages/registration_form/registrationForm.dart
@@ -0,0 +1,116 @@
+import 'package:avishkar/Screen/Pages/registration_form/Academicsdetails.dart';
+import 'package:avishkar/Screen/Pages/registration_form/projectdetails.dart';
+import 'package:avishkar/utils/app_text_form_field.dart';
+import 'package:flutter/material.dart';
+
+import 'package:avishkar/Constants/app_heights.dart' as app_heights;
+import 'package:avishkar/Constants/app_widths.dart' as app_widths;
+
+class RegisrationFormPage extends StatefulWidget {
+  const RegisrationFormPage({super.key});
+
+  @override
+  State<RegisrationFormPage> createState() => _RegisrationFormPageState();
+}
+
+class _RegisrationFormPageState extends State<RegisrationFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    // Accessing MediaQuery for responsive layout
+    // Calculate the height and width of the screen.
+    var media = MediaQuery.of(context);
+    final double screenHeight =
+        media.size.height - media.padding.top - media.padding.bottom;
+    final double screenWidth =
+        media.size.width - media.padding.left - media.padding.right;
+
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: screenHeight * app_heights.height80,
+                ),
+                TextFormFields(
+                    hintText: "Vishal",
+                    labelText: "First Name",
+                    onSaved: (value) {}),
+                SizedBox(
+                  height: screenHeight * app_heights.height80,
+                ),
+                TextFormFields(
+                    hintText: "Test",
+                    labelText: "Middle Name",
+                    onSaved: (value) {}),
+                SizedBox(
+                  height: screenHeight * app_heights.height80,
+                ),
+                TextFormFields(
+                    hintText: "Test",
+                    labelText: "Last Name",
+                    onSaved: (value) {}),
+                SizedBox(
+                  height: screenHeight * app_heights.height80,
+                ),
+                TextFormFields(
+                    hintText: "Test",
+                    labelText: "Father/Mother Name",
+                    onSaved: (value) {}),
+                SizedBox(
+                  height: screenHeight * app_heights.height80,
+                ),
+                TextFormFields(
+                    hintText: "example123@gmail.com",
+                    labelText: "Email",
+                    onSaved: (value) {}),
+                SizedBox(
+                  height: screenHeight * app_heights.height80,
+                ),
+                TextFormFields(
+                    hintText: "1234567890",
+                    labelText: "Mobile Number",
+                    onSaved: (value) {}),
+                SizedBox(
+                  height: screenHeight * app_heights.height80,
+                ),
+                TextFormFields(
+                    hintText: "1234567890",
+                    labelText: "Father/Mother Mobile NO.",
+                    onSaved: (value) {}),
+                SizedBox(
+                  height: screenHeight * app_heights.height80,
+                ),
+                TextFormFields(
+                    hintText: "19-03-2002",
+                    labelText: "DOB",
+                    onSaved: (value) {}),
+                SizedBox(
+                  height: screenHeight * app_heights.height80,
+                ),
+                TextFormFields(
+                    hintText: "e.g Lonere",
+                    labelText: "Residential Address",
+                    onSaved: (value) {}),
+                SizedBox(
+                  height: screenHeight * app_heights.height80,
+                ),
+                FloatingActionButton(
+                  child: Text("Next",style: TextStyle(color: Colors.black),),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => AcademicsDetailsPage(),
+                        ));
+                  },
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
